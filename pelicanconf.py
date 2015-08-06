70fd371380bc5c8468fd875d3ba2b286be17c562
Sam Frances <sam@samfrances.uk>
2015-08-06 21:48:42 +0100

    Change social links from defaults

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,12 +22,12 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('stackoverflow', 'http://stackoverflow.com/users/1256529/samfrances'),
+          ('github', 'http://github.com/samfrances'),
+          ('codewars', 'http://codewars.com/users/samfrances'),)
 
 DEFAULT_PAGINATION = 10
 
