ea984c84b0fd3a9386f433d1aec6db212e0e9c80
Sam Frances <sam@samfrances.uk>
2018-05-05 22:22:24 +0100

    Adapt fabric commands for github deploy

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,9 @@
 from fabric.api import *
-import fabric.contrib.project as project
 import os
 import shutil
 import sys
 import SocketServer
-from datetime import datetime # User addition
-import confidential # User addition
+from datetime import datetime  # User addition
 from uuid import uuid4
 
 from pelican.server import ComplexHTTPRequestHandler
@@ -16,26 +14,16 @@ import pelicanconf
 env.deploy_path = 'output'
 DEPLOY_PATH = env.deploy_path
 
-# Remote server configuration
-production = confidential.production
-dest_path = confidential.dest_path
-
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = 'my_rackspace_username'
-env.cloudfiles_api_key = 'my_rackspace_api_key'
-env.cloudfiles_container = 'my_cloudfiles_container'
-
-# Github Pages configuration
-env.github_pages_branch = "gh-pages"
-
 # Port for `serve`
 PORT = 8000
+
 
 def clean():
     """Remove generated files"""
     if os.path.isdir(DEPLOY_PATH):
         shutil.rmtree(DEPLOY_PATH)
         os.makedirs(DEPLOY_PATH)
+
 
 def build(publish=False):
     """Build local version of site"""
@@ -56,14 +44,12 @@ def build(publish=False):
         blog_index=blog_index.format(slug="blog"),
     ))
 
+
 def rebuild(publish=False):
     """`clean` then `build`"""
     clean()
     build(publish=publish)
 
-#def regenerate():
-#    """Automatically regenerate site upon file modification"""
-#    local('pelican -r -s pelicanconf.py')
 
 def serve():
     """Serve site at http://localhost:8000/"""
@@ -77,41 +63,11 @@ def serve():
     sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
     server.serve_forever()
 
+
 def reserve():
     """`build`, then `serve`"""
     build()
     serve()
-
-#def preview():
-#    """Build production version of site"""
-#    local('pelican -s publishconf.py')
-
-#def cf_upload():
-#    """Publish to Rackspace Cloud Files"""
-#    rebuild()
-#    with lcd(DEPLOY_PATH):
-#        local('swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-#              '-U {cloudfiles_username} '
-#              '-K {cloudfiles_api_key} '
-#              'upload -c {cloudfiles_container} .'.format(**env))
-
-@hosts(production)
-def publish():
-    """Publish to production via rsync"""
-    rebuild(publish=True)
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True,
-        extra_opts='-c',
-    )
-
-#def gh_pages():
-#    """Publish to GitHub Pages"""
-#    rebuild()
-#    local("ghp-import -b {github_pages_branch} {deploy_path}".format(**env))
-#    local("git push origin {github_pages_branch}".format(**env))
 
 
 # Extra utility functions
@@ -132,6 +88,7 @@ Status:draft
 
 """
 
+
 def make_entry(title):
     today = datetime.today()
     slug = title.lower().strip().replace(' ', '-')
@@ -148,5 +105,3 @@ def make_entry(title):
     with open(f_create, 'w') as w:
         w.write(t)
     print("File created -> " + f_create)
-
-
--- publish_gh.sh
+++ publish_gh.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# http://clontz.org/blog/2014/05/08/git-subtree-push-for-deployment/
 git push origin `git subtree split --prefix output src`:master --force
